66b4ff025b81a8f122652a92dc66cc2b23f9d9bf
deleav <a42006310@gmail.com>
2016-03-19 06:24:33 +0800

    code some parse

--- main.cpp
+++ main.cpp
@@ -88,6 +88,13 @@ bool IsTable2( string str ) {
   return false;
 } // IsTable2()
 
+bool IsBoolOperator( Token &token ) {
+  if ( token.mToken == "=" || token.mToken == "<>" || token.mToken == ">" ||
+       token.mToken == "<" || token.mToken == ">=" || token.mToken == "<=" )
+    return true;
+  return false;
+} // IsOperator()
+
 // /////////////////////////////////////////////////////////////////////////////
 //                                 Print                                      //
 // /////////////////////////////////////////////////////////////////////////////
@@ -196,47 +203,213 @@ void GetToken() {
 } // GetToken()
 
 // /////////////////////////////////////////////////////////////////////////////
+//                             FunctionReference                              //
+// /////////////////////////////////////////////////////////////////////////////
+
+bool ArithExp( Token &token );
+bool IDENT( Token &token );
+
+// /////////////////////////////////////////////////////////////////////////////
+//                               Sub3Command                                  //
+// /////////////////////////////////////////////////////////////////////////////
+
+bool SIGN( Token &token ) {
+  PrintNowFunction( "SIGN" );
+
+  return false;
+} // SIGN()
+
+bool NUM( Token &token ) {
+  PrintNowFunction( "NUM" );
+
+  return false;
+} // NUM()
+
+// /////////////////////////////////////////////////////////////////////////////
+//                               Sub2Command                                  //
+// /////////////////////////////////////////////////////////////////////////////
+
+bool Factor( Token &token ) {
+  PrintNowFunction( "Factor" );
+  if ( SIGN( token ) ) {
+    if ( NextToken( token ) ) {
+      if ( NUM( token ) ) {
+        return true;
+      } // if
+    } // if
+  } // if
+  else if ( NUM( token ) ) {
+    return true;
+  } // else if
+  else if ( IDENT( token ) ) {
+    return true;
+  } // else if
+  else if ( token.mToken == "(" ) {
+    if ( NextToken( token ) ) {
+      if ( ArithExp( token ) ) {
+        if ( NextToken( token ) ) {
+          if ( token.mToken == ")" ) {
+            return true;
+          } // if
+        } // if
+      } // if
+    } // if
+  } // else if
+
+  return false;
+} // Factor()
+
+// /////////////////////////////////////////////////////////////////////////////
+//                                SubCommand                                  //
+// /////////////////////////////////////////////////////////////////////////////
+
+bool IDENT( Token &token ) {
+  PrintNowFunction( "IDENT" );
+
+  return false;
+} // IDENT()
+
+bool Term( Token &token ) {
+  PrintNowFunction( "Term" );
+  if ( Factor( token ) ) {
+    return true;
+  } // if
+  else if ( Term( token ) ) {
+    if ( NextToken( token ) ) {
+      if ( token.mToken == "*" ) {
+        if ( NextToken( token ) ) {
+          if ( Factor( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // if
+      else if ( token.mToken == "/" ) {
+        if ( NextToken( token ) ) {
+          if ( Factor( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // else if
+    } // if
+  } // else if
+
+  return false;
+} // Term()
+
+// /////////////////////////////////////////////////////////////////////////////
 //                                 Command                                    //
 // /////////////////////////////////////////////////////////////////////////////
 
-bool AirthExp() {
-
-  return false;
-} // AirthExp()
-
-bool BooleanExp() {
-
-  return false;
-} // BooleanExp()
-
-bool Statement() {
-
-  return false;
-} // statement()
-
-bool QUIT() {
+bool QUIT( Token &token ) {
+  PrintNowFunction( "QUIT" );
+  if ( token.mToken == "quit" || token.mToken == "QUIT" )
+    return true;
 
   return false;
 } // QUIT()
 
-bool Command() {
-  if ( Statement() ) {
+bool ArithExp( Token &token ) {
+  PrintNowFunction( "ArithExp" );
+  if ( Term( token ) ) {
     return true;
   } // if
-  else if ( BooleanExp() ) {
-    return true;
-  } // else if
-  else if ( AirthExp() ) {
-    return true;
+  else if ( ArithExp( token ) ) {
+    if ( NextToken( token ) ) {
+      if ( token.mToken == "+" ) {
+        if ( NextToken( token ) ) {
+          if ( Term( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // if
+      else if ( token.mToken == "-" ) {
+        if ( NextToken( token ) ) {
+          if ( Term( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // else if
+    } // if
   } // else if
 
+  return false;
+} // ArithExp()
+
+bool BooleanExp( Token &token ) {
+  PrintNowFunction( "BooleanExp" );
+  if ( ArithExp( token ) ) {
+    if ( NextToken( token ) ) {
+      if ( IsBoolOperator( token ) ) {
+        if ( NextToken( token ) ) {
+          if ( ArithExp( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // if
+    } // if
+  } // if
+
+  return false;
+} // BooleanExp()
+
+bool Statement( Token &token ) {
+  PrintNowFunction( "Statement" );
+  if ( IDENT( token ) ) {
+    if ( NextToken( token ) ) {
+      if ( token.mToken == ":=" ) {
+        if ( NextToken( token ) ) {
+          if ( ArithExp( token ) ) {
+            return true;
+          } // if
+        } // if
+      } // if
+    } // if
+  } // if
+
+  return false;
+} // statement()
+
+bool Command( string &e ) {
+  PrintNowFunction( "Command" );
+  Token token;
+  while( NextToken( token ) ) {
+    if ( QUIT( token ) ) {
+      return false;
+    } // if
+    else if ( Statement( token ) ) {
+      if ( NextToken( token ) ) {
+        if ( token.mToken == ";" ) {
+          return true;
+        } // if
+      } // if
+    } // if
+    // else if ( BooleanExp( token ) ) {
+    //   if ( NextToken( token ) ) {
+    //     if ( token.mToken == ";" ) {
+    //       return true;
+    //     } // if
+    //   } // if
+    // } // else if
+    // else if ( ArithExp( token ) ) {
+    //   if ( NextToken( token ) ) {
+    //     if ( token.mToken == ";" ) {
+    //       return true;
+    //     } // if
+    //   } // if
+    // } // else if
+  } // while
+
+  e = "Something Error";
   return false;
 } // Command()
 
 
 int main() {
-  GetToken();
-
+  string e;
+  // GetToken();
+  while( Command( e ) );
+  if ( e.size() > 0 )
+    cout << e;
 
   return 0;
 } // main()
